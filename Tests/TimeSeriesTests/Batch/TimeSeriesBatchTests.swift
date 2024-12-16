dfefcf535413f973a1d83276190a33b141ec2baf
vitali-kurlovich <vitalikurlovich@gmail.com>
2024-12-16 20:13:09 +0400

    Update TimeSeriesBatchTests.swift

--- Tests/TimeSeriesTests/Batch/TimeSeriesBatchTests.swift
+++ Tests/TimeSeriesTests/Batch/TimeSeriesBatchTests.swift
@@ -34,6 +34,7 @@ struct TimeSeriesBatchTests {
         let batch = TimeSeriesBatch(CollectionOfOne(series))
 
         #expect(batch.isEmpty == false)
+        #expect(batch.count == 1)
         #expect(batch.timeRange == FixedDateInterval(start: FixedDate(110), end: FixedDate(110)))
 
         let interval = FixedDateInterval(start: FixedDate(100), end: FixedDate(400))
@@ -45,4 +46,89 @@ struct TimeSeriesBatchTests {
         #expect(batch[FixedDateInterval(start: FixedDate(200), end: FixedDate(400))].isEmpty == true)
         #expect(batch[FixedDateInterval(start: FixedDate(200), end: FixedDate(400))].timeRange == nil)
     }
+
+    @Test("Many items batch")
+    func batch() {
+        let series1 = Self.series1
+        let series2 = Self.series2
+        let series3 = Self.series3
+
+        let batch = TimeSeriesBatch([series1, series2, series3])
+
+        #expect(batch.isEmpty == false)
+        #expect(batch.count == 3)
+        #expect(batch.timeRange == FixedDateInterval(start: FixedDate(110), end: FixedDate(1150)))
+
+        #expect(batch[0] == series1)
+        #expect(batch[1] == series2)
+        #expect(batch[2] == series3)
+    }
+
+    @Test("Sub batch")
+    func subBatch() {
+        let series1 = Self.series1
+        let series2 = Self.series2
+        let series3 = Self.series3
+
+        let batch = TimeSeriesBatch([series1, series2, series3])
+
+        #expect(batch[FixedDateInterval(start: FixedDate(90), end: FixedDate(100))].isEmpty)
+        #expect(batch[FixedDateInterval(start: FixedDate(500), end: FixedDate(600))].isEmpty)
+        #expect(batch[FixedDateInterval(start: FixedDate(2000), end: FixedDate(2400))].isEmpty)
+
+        let renge = FixedDateInterval(start: FixedDate(90), end: FixedDate(250))
+
+        let expacted = TimeSeriesBatch([series1[renge], series2[renge]])
+
+        #expect(batch[renge] == expacted)
+        
+        
+        let renge1 = FixedDateInterval(start: FixedDate(900), end: FixedDate(1100))
+        let expacted1 = TimeSeriesBatch([series3[renge1]])
+        
+        #expect(batch[renge1] == expacted1)
+        
+    }
+}
+
+private
+extension TimeSeriesBatchTests {
+    static let series1 = TimeSeries(timeBase: FixedDate(100),
+                                    items: [
+                                        MocItem(time: 10, index: 1),
+                                    ])
+
+    static let series2 = TimeSeries(timeBase: FixedDate(200),
+                                    items: [
+                                        MocItem(time: 10, index: 1),
+                                        MocItem(time: 20, index: 2),
+                                        MocItem(time: 30, index: 3),
+                                        MocItem(time: 40, index: 4),
+                                        MocItem(time: 50, index: 5),
+                                        MocItem(time: 60, index: 6),
+                                        MocItem(time: 70, index: 7),
+                                        MocItem(time: 80, index: 8),
+                                        MocItem(time: 90, index: 9),
+                                        MocItem(time: 100, index: 10),
+                                        MocItem(time: 110, index: 11),
+                                    ])
+
+    static let series3 = TimeSeries(timeBase: FixedDate(1000),
+                                    items: [
+                                        MocItem(time: 10, index: 1),
+                                        MocItem(time: 20, index: 2),
+                                        MocItem(time: 30, index: 3),
+                                        MocItem(time: 40, index: 4),
+                                        MocItem(time: 50, index: 5),
+                                        MocItem(time: 60, index: 6),
+                                        MocItem(time: 70, index: 7),
+                                        MocItem(time: 80, index: 8),
+                                        MocItem(time: 90, index: 9),
+                                        MocItem(time: 100, index: 10),
+                                        MocItem(time: 110, index: 11),
+                                        MocItem(time: 120, index: 12),
+                                        MocItem(time: 130, index: 13),
+                                        MocItem(time: 140, index: 14),
+                                        MocItem(time: 150, index: 15),
+                                    ])
 }
