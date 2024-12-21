391fd0f4429ef8ee2fd98d93831be6f175a51629
vitali-kurlovich <vitalikurlovich@gmail.com>
2024-12-21 10:13:18 +0400

    Update TimeSeriesCollection.swift

--- Sources/TimeSeries/TimeSeriesCollection.swift
+++ Sources/TimeSeries/TimeSeriesCollection.swift
@@ -7,36 +7,15 @@
 
 import Algorithms
 
-public protocol TimeSeriesCollection: RandomAccessCollection
+public protocol TimeSeriesCollection: RandomAccessCollection, FixedDateTimeBased
     where Element: TimeSeriesItem,
     SubSequence: TimeSeriesCollection
 {
-    var timeBase: FixedDate { get }
-
     static var empty: Self { get }
 }
 
 public
 extension TimeSeriesCollection {
-    var timeRange: FixedDateInterval? {
-        if isEmpty {
-            return nil
-        }
-
-        let start = dateTime(at: startIndex)
-        let end = dateTime(at: index(before: endIndex))
-
-        return FixedDateInterval(start: start, end: end)
-    }
-}
-
-public
-extension TimeSeriesCollection {
-    func dateTime(at index: Index) -> FixedDate {
-        let time = self[index].time
-        return timeBase.adding(milliseconds: time)
-    }
-
     subscript(_ range: FixedDateInterval) -> Self.SubSequence {
         guard let timeRange, timeRange.intersects(range) else {
             return .empty
