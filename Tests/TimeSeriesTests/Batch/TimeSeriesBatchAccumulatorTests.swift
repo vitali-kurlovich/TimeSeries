c006cef9d4cf53f896ac2639cc6362257877cf54
vitali-kurlovich <vitalikurlovich@gmail.com>
2024-12-25 00:17:49 +0400

    Update TimeSeriesBatchAccumulatorTests.swift

--- Tests/TimeSeriesTests/Batch/TimeSeriesBatchAccumulatorTests.swift
+++ Tests/TimeSeriesTests/Batch/TimeSeriesBatchAccumulatorTests.swift
@@ -44,7 +44,7 @@ private enum TestsData {
     }
 
     static var batchOfOneItem: Batch {
-        Batch([.init(timeBase: FixedDate(100), items: [.init(time: 20, index: 0)])])
+        Batch([.init(timeBase: FixedDate(120), items: [.init(time: 0, index: 0)])])
     }
 
     static var items_8: [AccumulatorItem] {
@@ -61,16 +61,16 @@ private enum TestsData {
     }
 
     static var batchOf_8_Item: Batch {
-        Batch([.init(timeBase: FixedDate(100), items:
+        Batch([.init(timeBase: FixedDate(110), items:
             [
-                .init(time: 10, index: 0),
-                .init(time: 20, index: 1),
-                .init(time: 30, index: 2),
-                .init(time: 40, index: 3),
-                .init(time: 150, index: 4),
-                .init(time: 160, index: 5),
-                .init(time: 170, index: 6),
-                .init(time: 180, index: 7),
+                .init(time: 0, index: 0),
+                .init(time: 10, index: 1),
+                .init(time: 20, index: 2),
+                .init(time: 30, index: 3),
+                .init(time: 140, index: 4),
+                .init(time: 150, index: 5),
+                .init(time: 160, index: 6),
+                .init(time: 170, index: 7),
             ])])
     }
 
@@ -91,22 +91,77 @@ private enum TestsData {
 
     static var batchOf_10_Item: Batch {
         Batch([
-            .init(timeBase: FixedDate(100), items: [
-                .init(time: 10, index: 0),
-                .init(time: 20, index: 1),
-                .init(time: 30, index: 2),
-                .init(time: 40, index: 3),
-                .init(time: 150, index: 4),
-                .init(time: 160, index: 5),
-                .init(time: 170, index: 6),
-                .init(time: 180, index: 7),
+            .init(timeBase: FixedDate(110), items: [
+                .init(time: 0, index: 0),
+                .init(time: 10, index: 1),
+                .init(time: 20, index: 2),
+                .init(time: 30, index: 3),
+                .init(time: 140, index: 4),
+                .init(time: 150, index: 5),
+                .init(time: 160, index: 6),
+                .init(time: 170, index: 7),
             ]),
 
-            .init(timeBase: FixedDate(200), items: [
-                .init(time: 90, index: 8),
-                .init(time: 200, index: 9),
+            .init(timeBase: FixedDate(290), items: [
+                .init(time: 0, index: 8),
+                .init(time: 110, index: 9),
             ]),
 
         ])
     }
+
+    static var items_10_reverse: [AccumulatorItem] {
+        [
+            .init(timeBase: FixedDate(300), item: .init(time: 100, index: 9)),
+            .init(timeBase: FixedDate(200), item: .init(time: 90, index: 8)),
+            .init(timeBase: FixedDate(200), item: .init(time: 80, index: 7)),
+            .init(timeBase: FixedDate(200), item: .init(time: 70, index: 6)),
+            .init(timeBase: FixedDate(200), item: .init(time: 60, index: 5)),
+            .init(timeBase: FixedDate(200), item: .init(time: 50, index: 4)),
+            .init(timeBase: FixedDate(100), item: .init(time: 40, index: 3)),
+            .init(timeBase: FixedDate(100), item: .init(time: 30, index: 2)),
+            .init(timeBase: FixedDate(100), item: .init(time: 20, index: 1)),
+            .init(timeBase: FixedDate(100), item: .init(time: 10, index: 0)),
+        ]
+    }
+
+    static var batchOf_10_Item_reverse: Batch {
+        Batch([
+            .init(timeBase: FixedDate(110), items: [
+                .init(time: 0, index: 0),
+                .init(time: 10, index: 1),
+                .init(time: 20, index: 2),
+                .init(time: 30, index: 3),
+                .init(time: 140, index: 4),
+                .init(time: 150, index: 5),
+                .init(time: 160, index: 6),
+                .init(time: 170, index: 7),
+            ]),
+
+            .init(timeBase: FixedDate(290), items: [
+                .init(time: 0, index: 8),
+                .init(time: 110, index: 9),
+            ]),
+
+        ])
+    }
+
+    // Overflow
+
+    static var items_overflow: [AccumulatorItem] {
+        [
+            .init(timeBase: FixedDate(100), item: .init(time: 32767, index: 0)),
+            .init(timeBase: FixedDate(100), item: .init(time: 20, index: 1)),
+            .init(timeBase: FixedDate(100), item: .init(time: 30, index: 2)),
+            .init(timeBase: FixedDate(100), item: .init(time: 40, index: 3)),
+            .init(timeBase: FixedDate(200), item: .init(time: 50, index: 4)),
+            .init(timeBase: FixedDate(200), item: .init(time: 60, index: 5)),
+            .init(timeBase: FixedDate(200), item: .init(time: 70, index: 6)),
+            .init(timeBase: FixedDate(200), item: .init(time: 80, index: 7)),
+            .init(timeBase: FixedDate(200), item: .init(time: 90, index: 8)),
+            .init(timeBase: FixedDate(300), item: .init(time: 100, index: 9)),
+        ]
+    }
 }
+
+// 32767
