8c9a49d1c4409d1377997487119e2f53123cda28
vitali-kurlovich <vitalikurlovich@gmail.com>
2024-12-25 21:23:23 +0400

    Update TimeSeriesBatchAccumulatorTests.swift

--- Tests/TimeSeriesTests/Batch/TimeSeriesBatchAccumulatorTests.swift
+++ Tests/TimeSeriesTests/Batch/TimeSeriesBatchAccumulatorTests.swift
@@ -18,6 +18,7 @@ struct TimeSeriesBatchAccumulatorTests {
               (TestsData.oneItem, TestsData.batchOfOneItem),
               (TestsData.items_8, TestsData.batchOf_8_Item),
               (TestsData.items_10, TestsData.batchOf_10_Item),
+              (TestsData.items_10_reverse, TestsData.batchOf_10_Item_reverse),
           ])
     func accumulate(items: [AccumulatorItem], expected: Batch) throws {
         var accumulator = TimeSeriesBatchAccumulator<MocItem>(batchSize: 8)
@@ -112,16 +113,17 @@ private enum TestsData {
 
     static var items_10_reverse: [AccumulatorItem] {
         [
-            .init(timeBase: FixedDate(300), item: .init(time: 100, index: 9)),
-            .init(timeBase: FixedDate(200), item: .init(time: 90, index: 8)),
-            .init(timeBase: FixedDate(200), item: .init(time: 80, index: 7)),
-            .init(timeBase: FixedDate(200), item: .init(time: 70, index: 6)),
-            .init(timeBase: FixedDate(200), item: .init(time: 60, index: 5)),
-            .init(timeBase: FixedDate(200), item: .init(time: 50, index: 4)),
-            .init(timeBase: FixedDate(100), item: .init(time: 40, index: 3)),
-            .init(timeBase: FixedDate(100), item: .init(time: 30, index: 2)),
-            .init(timeBase: FixedDate(100), item: .init(time: 20, index: 1)),
-            .init(timeBase: FixedDate(100), item: .init(time: 10, index: 0)),
+            .init(timeBase: FixedDate(300), item: .init(time: 100, index: 9)), // 0
+            .init(timeBase: FixedDate(200), item: .init(time: 90, index: 8)), // 1
+            .init(timeBase: FixedDate(200), item: .init(time: 80, index: 7)), // 2
+            .init(timeBase: FixedDate(200), item: .init(time: 70, index: 6)), // 3
+            .init(timeBase: FixedDate(200), item: .init(time: 60, index: 5)), // 4
+            .init(timeBase: FixedDate(200), item: .init(time: 50, index: 4)), // 5
+            .init(timeBase: FixedDate(100), item: .init(time: 40, index: 3)), // 6
+            .init(timeBase: FixedDate(100), item: .init(time: 30, index: 2)), // 7
+
+            .init(timeBase: FixedDate(100), item: .init(time: 20, index: 1)), // 8
+            .init(timeBase: FixedDate(100), item: .init(time: 10, index: 0)), // 9
         ]
     }
 
@@ -130,17 +132,17 @@ private enum TestsData {
             .init(timeBase: FixedDate(110), items: [
                 .init(time: 0, index: 0),
                 .init(time: 10, index: 1),
-                .init(time: 20, index: 2),
-                .init(time: 30, index: 3),
-                .init(time: 140, index: 4),
-                .init(time: 150, index: 5),
-                .init(time: 160, index: 6),
-                .init(time: 170, index: 7),
             ]),
 
-            .init(timeBase: FixedDate(290), items: [
-                .init(time: 0, index: 8),
-                .init(time: 110, index: 9),
+            .init(timeBase: FixedDate(130), items: [
+                .init(time: 0, index: 2),
+                .init(time: 10, index: 3),
+                .init(time: 120, index: 4),
+                .init(time: 130, index: 5),
+                .init(time: 140, index: 6),
+                .init(time: 150, index: 7),
+                .init(time: 160, index: 8),
+                .init(time: 270, index: 9),
             ]),
 
         ])
