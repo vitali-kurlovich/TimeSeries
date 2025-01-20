4067139675f9251df00c17824ffa9e892dac46f4
vitali-kurlovich <vitalikurlovich@gmail.com>
2025-01-21 01:41:29 +0400

    Update TimeSeriesCollectionTests.swift

--- Tests/TimeSeriesTests/TimeSeries/TimeSeriesCollectionTests.swift
+++ Tests/TimeSeriesTests/TimeSeries/TimeSeriesCollectionTests.swift
@@ -112,5 +112,51 @@ enum TimeSeriesCollectionTests {
             #expect(series[FixedDateInterval(start: FixedDate(110), end: FixedDate(150))] == series[0 ... 3])
             #expect(series[FixedDateInterval(start: FixedDate(111), end: FixedDate(139))] == series[1 ... 2])
         }
+
+        @Test("Split maxCount", arguments: [
+            TestsData(series: TestsData.series, maxCount: 12, expected: [TestsData.series[0 ... 11]]),
+
+            TestsData(series: TestsData.series, maxCount: 4, expected: [
+                TestsData.series[0 ... 3],
+                TestsData.series[4 ... 7],
+                TestsData.series[8 ... 11],
+            ]),
+
+            TestsData(series: TestsData.series, maxCount: 8, expected: [
+                TestsData.series[0 ... 7],
+                TestsData.series[8 ... 11],
+            ]),
+
+        ])
+        fileprivate func split(_ testData: TestsData) {
+            let split = testData.series.split(maxCount: testData.maxCount)
+
+            #expect(split == testData.expected)
+        }
     }
+}
+
+private struct TestsData {
+    let series: TimeSeries<MocItem>
+    let maxCount: Int
+    let expected: [TimeSeries<MocItem>.SubSequence]
+
+    static let series = TimeSeries(timeBase: FixedDate(100),
+                                   items: Self.seriesItems)
+    static let seriesItems: [MocItem] = [
+        MocItem(time: 10, index: 1), // 0
+        MocItem(time: 20, index: 2), // 1
+        MocItem(time: 30, index: 3), // 2
+        MocItem(time: 40, index: 4), // 3
+
+        MocItem(time: 100, index: 1), // 4
+        MocItem(time: 200, index: 2), // 5
+        MocItem(time: 300, index: 3), // 6
+        MocItem(time: 400, index: 4), // 7
+
+        MocItem(time: 500, index: 1), // 8
+        MocItem(time: 600, index: 2), // 9
+        MocItem(time: 700, index: 3), // 10
+        MocItem(time: 800, index: 4), // 11
+    ]
 }
