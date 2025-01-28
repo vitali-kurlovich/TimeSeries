15441512f0094b24d236f0250675c374a3215c29
vitali-kurlovich <vitalikurlovich@gmail.com>
2025-01-28 23:36:13 +0300

    Update TimeSeriesCollectionTests+Slice.swift

--- Tests/TimeSeriesTests/TimeSeriesCollection/TimeSeriesCollectionTests+Slice.swift
+++ Tests/TimeSeriesTests/TimeSeriesCollection/TimeSeriesCollectionTests+Slice.swift
@@ -50,78 +50,49 @@ extension TimeSeriesCollectionTests {
             #expect(series[FixedDate(111) ... FixedDate(139)] == series[1 ... 2])
         }
 
-        @Test("Split maxCount",
-              arguments: [
-                  (series: TestsData.series, maxCount: 12, expected: [TestsData.series[0 ... 11]]),
+//        @Test("Split by time omittingEmpty",
+//              arguments: [
+//                  (series: TestsData.series, time: FixedDate(100), expected: [TestsData.series[0 ..< 12]]),
+//                  (series: TestsData.series, time: FixedDate(110), expected: [TestsData.series[0 ..< 1], TestsData.series[1 ..< 12]]),
+//                  (series: TestsData.series, time: FixedDate(140), expected: [TestsData.series[0 ... 3], TestsData.series[4 ..< 12]]),
+//                  (series: TestsData.series, time: FixedDate(150), expected: [TestsData.series[0 ... 3], TestsData.series[4 ..< 12]]),
+//                  (series: TestsData.series, time: FixedDate(900), expected: [TestsData.series[0 ... 10], TestsData.series[11 ..< 12]]),
+//              ])
+//        func split(_ test: (series: TimeSeries<MocItem>, time: FixedDate, expected: [TimeSeries<MocItem>.SubSequence])) {
+//            let split = test.series.split(test.time, omittingEmptySubsequences: true)
+//            #expect(split == test.expected)
+//        }
 
-                  (series: TestsData.series, maxCount: 4, expected: [
-                      TestsData.series[0 ... 3],
-                      TestsData.series[4 ... 7],
-                      TestsData.series[8 ... 11],
-                  ]),
+        /*
+         @Test("Split by time", arguments: [
+             (series: TestsData.series,
+              time: FixedDate(100),
+              omittingEmpty: false,
+              expected: [TestsData.series[0 ... 11]]),
 
-                  (series: TestsData.series, maxCount: 8, expected: [
-                      TestsData.series[0 ... 7],
-                      TestsData.series[8 ... 11],
-                  ]),
+             (series: TestsData.series, time: FixedDate(100), omittingEmpty: true, expected: [TestsData.series[0 ... 11]]),
 
-              ])
-        func split(_ testData: (series: TimeSeries<MocItem>, maxCount: Int, expected: [TimeSeries<MocItem>.SubSequence])) {
-            let split = testData.series.split(maxCount: testData.maxCount)
+             (series: TestsData.series, time: FixedDate(900), omittingEmpty: false, expected: [
+                 TestsData.series[0 ..< 12],
+                 .empty.setTimeBase(FixedDate(900))
 
-            #expect(split == testData.expected)
-        }
+             ]),
 
-        @Test("Split by time", arguments: [
-            (series: TestsData.series,
-             time: FixedDate(100),
-             omittingEmpty: false,
-             expected: [TestsData.series[0 ... 11]]),
+             (series: TestsData.series, time: FixedDate(900), omittingEmpty: true, expected: [
+                 TestsData.series[0 ..< 12],
 
-            (series: TestsData.series, time: FixedDate(100), omittingEmpty: true, expected: [TestsData.series[0 ... 11]]),
+             ]),
 
-            (series: TestsData.series, time: FixedDate(900), omittingEmpty: false, expected: [
-                TestsData.series[0 ..< 12],
-                .empty.setTimeBase(FixedDate(900))
+         ])
+         func split(_ testData: (series: TimeSeries<MocItem>,
+                                 time: FixedDate,
+                                 omittingEmpty: Bool,
+                                 expected: [TimeSeries<MocItem>.SubSequence]))
+         {
+             let split = testData.series.split(testData.time, omittingEmptySubsequences: testData.omittingEmpty)
 
-            ]),
-
-            (series: TestsData.series, time: FixedDate(900), omittingEmpty: true, expected: [
-                TestsData.series[0 ..< 12],
-
-            ]),
-
-        ])
-        func split(_ testData: (series: TimeSeries<MocItem>,
-                                time: FixedDate,
-                                omittingEmpty: Bool,
-                                expected: [TimeSeries<MocItem>.SubSequence]))
-        {
-            let split = testData.series.split(testData.time, omittingEmptySubsequences: testData.omittingEmpty)
-
-            #expect(split == testData.expected)
-        }
+             #expect(split == testData.expected)
+         }
+         */
     }
-}
-
-private enum TestsData {
-    static let series = TimeSeries(timeBase: FixedDate(100),
-                                   items: Self.seriesItems)
-
-    static let seriesItems: [MocItem] = [
-        MocItem(time: 10, index: 1), // 0
-        MocItem(time: 20, index: 2), // 1
-        MocItem(time: 30, index: 3), // 2
-        MocItem(time: 40, index: 4), // 3
-
-        MocItem(time: 100, index: 5), // 4
-        MocItem(time: 200, index: 6), // 5
-        MocItem(time: 300, index: 7), // 6
-        MocItem(time: 400, index: 8), // 7
-
-        MocItem(time: 500, index: 9), // 8
-        MocItem(time: 600, index: 10), // 9
-        MocItem(time: 700, index: 11), // 10
-        MocItem(time: 800, index: 12), // 11
-    ]
 }
